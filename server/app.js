be06b8fd2a5566f011f2c2a372949b712ee5ed57
kodemystic <btarrana@protonmail.ch>
2023-09-26 13:34:21 +0200

    1

--- server/app.js
+++ server/app.js
@@ -8,19 +8,20 @@ app.get('/api/hello', (req, res) => {
   res.send({ message: 'Hello from server!' });
 });
 
-// app.get('/', (req, res) => {
-//   res.send('Hello, world!');
-// });
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/public')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/public/index.html'));
+  res.send('Server running!');
 });
+
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/ !`);
 });
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '../client/public')));
+
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '../client/public/index.html'));
+// });
