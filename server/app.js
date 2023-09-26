1b513cb198303c505bed40962c373492acad1075
kodemystic <btarrana@protonmail.ch>
2023-09-26 13:33:21 +0200

    1

--- server/app.js
+++ server/app.js
@@ -13,12 +13,12 @@ app.get('/api/hello', (req, res) => {
 // });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+  res.sendFile(path.join(__dirname + '../client/public/index.html'));
 });
 
 app.listen(port, () => {
