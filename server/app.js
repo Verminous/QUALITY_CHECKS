c232b65446f113dd12f6f329521cf39ebbc0c0d7
kodemystic <btarrana@protonmail.ch>
2023-10-03 16:32:37 +0200

    1

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.SERV_PORT;
 const hostname = process.env.HOSTNAME;
 const upload = multer({ dest: "uploads/" });
+
 app.use(bodyParser.json());
 app.use(cors());
 app.get("/", (req, res) => {
@@ -64,122 +65,31 @@ app.post("/process", upload.single("file"), async (req, res) => {
     const workbook = xlsx.readFile(lastUploadedFilePath);
     const sheetName = workbook.SheetNames[0];
     const originalXlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    const incidentsByAgent = {};
     const incidentConfigs = config.incidentConfigs;
-    originalXlData.forEach((incident) => {
-      const agent = incident["Taken By"];
-      if (!incidentsByAgent[agent]) {
-        incidentsByAgent[agent] = [];
-      }
-      incidentsByAgent[agent].push(incident);
-    });
     const sfMembers = config.sfMembers;
-    const agents = Object.keys(incidentsByAgent);
-    const shuffledAgents = agents.sort(() => 0.5 - Math.random());
-    const sfAgentMapping = {};
-    shuffledAgents.forEach((agent, index) => {
-      const sfMember = sfMembers[index % sfMembers.length];
-      if (!sfAgentMapping[sfMember]) sfAgentMapping[sfMember] = [];
-      sfAgentMapping[sfMember].push(agent);
-    });
-    const selectedIncidents = {};
-    const maxIncidents = config.incidentsPerAgent || 10;
-    const processedTaskNumbers = new Set();
-    const processedTaskNumbersByAgent = {};
-
-    for (const sfMember in sfAgentMapping) {
-      selectedIncidents[sfMember] = {};
-
-      sfAgentMapping[sfMember].forEach((agent) => {
-        selectedIncidents[sfMember][agent] = [];
-
-        if (!processedTaskNumbersByAgent[agent]) {
-          processedTaskNumbersByAgent[agent] = new Set();
-        }
-
-        for (const incidentConfig of incidentConfigs) {
-          let currentService = incidentConfig.service !== "RANDOM" ? incidentConfig.service : getRandomService();
-          let currentContactType = incidentConfig.contactType !== "RANDOM" ? incidentConfig.contactType : getRandomContactType();
-          let currentFtf = incidentConfig.ftf !== "RANDOM" ? incidentConfig.ftf : getRandomFtf();
-
-          let potentialIncidents = incidentsByAgent[agent].filter((incident) => {
-            return (
-              !processedTaskNumbersByAgent[agent].has(incident["Task Number"]) &&
-              currentService === incident["Service"] &&
-              currentContactType === incident["Contact type"] &&
-              currentFtf === incident["First time fix"]
-            );
-          });
-
-          let incidentsToAssign = Math.min(potentialIncidents.length, maxIncidents - selectedIncidents[sfMember][agent].length);
-          for (let i = 0; i < incidentsToAssign; i++) {
-            let incident = potentialIncidents[i];
-            processedTaskNumbersByAgent[agent].add(incident["Task Number"]);
-            selectedIncidents[sfMember][agent].push(incident);
-          }
-        }
-      });
-    }
-    const rows = [];
-    let previousSFMember = "";
-    let previousAgent = "";
-    for (const sfMember in selectedIncidents) {
-      for (const agent in selectedIncidents[sfMember]) {
-        for (
-          let i = 0;
-          i < incidentConfigs.length &&
-          selectedIncidents[sfMember][agent].length < maxIncidents;
-          i++
-        ) {
-          const incidentConfig = incidentConfigs[i];
-          const filteredIncidents = originalXlData.filter((incident) => {
-            return (
-              (incidentConfig.service === "RANDOM" ||
-                incidentConfig.service === incident["Service"]) &&
-              (incidentConfig.contactType === "RANDOM" ||
-                incidentConfig.contactType === incident["Contact type"]) &&
-              (incidentConfig.ftf === "RANDOM" ||
-                incidentConfig.ftf === incident["First time fix"]) &&
-              !processedTaskNumbers.has(incident["Task Number"])
-            );
-          });
-          filteredIncidents.forEach((incident) => {
-            if (
-              selectedIncidents[sfMember][agent].length < maxIncidents &&
-              incident["Taken By"] === agent
-            ) {
-              processedTaskNumbers.add(incident["Task Number"]);
-              selectedIncidents[sfMember][agent].push(incident);
-            }
-          });
-        }
-        selectedIncidents[sfMember][agent].forEach((incident) => {
-          rows.push({
-            "SF Member": previousSFMember === sfMember ? "" : sfMember,
-            Agent: previousAgent === agent ? "" : agent,
-            "Task Number": incident["Task Number"],
-            Service: incident["Service"],
-            "Contact type": incident["Contact type"],
-            "First time fix": incident["First time fix"],
-          });
-          if (previousSFMember !== sfMember) previousSFMember = sfMember;
-          if (previousAgent !== agent) previousAgent = agent;
-        });
-      }
-    }
-    const newWorkbook = workbook;
+    const incidentsByAgent = mapIncidentsByAgent(originalXlData);
+    const sfAgentMapping = mapSFAgentsToIncidentAgents(
+      sfMembers,
+      incidentsByAgent
+    );
+    const selectedIncidents = await selectIncidentsByConfiguration(
+      originalXlData,
+      incidentConfigs,
+      config.incidentsPerAgent,
+      sfAgentMapping
+    );
+    const rows = formatRowsForDownload(selectedIncidents);
     if (rows.length < config.incidentsPerAgent) {
       console.error("Not enough incidents matched the provided configuration");
-      res
+      return res
         .status(500)
         .send("Not enough incidents matched the provided configuration");
-      return;
     }
     const newWorksheet = xlsx.utils.json_to_sheet(rows);
-    if (!newWorkbook.Sheets["Processed List"]) {
-      xlsx.utils.book_append_sheet(newWorkbook, newWorksheet, "Processed List");
+    if (!workbook.Sheets["Processed List"]) {
+      xlsx.utils.book_append_sheet(workbook, newWorksheet, "Processed List");
     } else {
-      newWorkbook.Sheets["Processed List"] = newWorksheet;
+      workbook.Sheets["Processed List"] = newWorksheet;
     }
     const newFilePath = path.join(
       __dirname,
@@ -187,7 +97,7 @@ app.post("/process", upload.single("file"), async (req, res) => {
       process.env.SERV_FILENAME
     );
     try {
-      xlsx.writeFile(newWorkbook, newFilePath);
+      xlsx.writeFile(workbook, newFilePath);
     } catch (error) {
       console.error("Error writing the workbook:", error);
     }
@@ -198,9 +108,118 @@ app.post("/process", upload.single("file"), async (req, res) => {
     });
   } catch (error) {
     console.error("Error in /process:", error);
+    console.error("Request body:", req.body);
+    if (lastUploadedFilePath) {
+      console.error("Last uploaded file path:", lastUploadedFilePath);
+    }
     res.status(500).send("Internal Server Error");
   }
 });
+function mapIncidentsByAgent(originalXlData) {
+  const incidentsByAgent = {};
+  originalXlData.forEach((incident) => {
+    const agent = incident["Taken By"];
+    if (!incidentsByAgent[agent]) {
+      incidentsByAgent[agent] = [];
+    }
+    incidentsByAgent[agent].push(incident);
+  });
+  return incidentsByAgent;
+}
+function mapSFAgentsToIncidentAgents(sfMembers, incidentsByAgent) {
+  const sfAgentMapping = {};
+  const shuffledAgents = Object.keys(incidentsByAgent).sort(
+    () => 0.5 - Math.random()
+  );
+  shuffledAgents.forEach((agent, index) => {
+    const sfMember = sfMembers[index % sfMembers.length];
+    if (!sfAgentMapping[sfMember]) sfAgentMapping[sfMember] = [];
+    sfAgentMapping[sfMember].push(agent);
+  });
+  return sfAgentMapping;
+}
+function selectIncidentsByConfiguration(
+  originalXlData,
+  incidentConfigs,
+  maxIncidents,
+  sfAgentMapping
+) {
+  const incidentsByAgent = {};
+  const selectedIncidents = {};
+  const processedTaskNumbersByAgent = {}; 
+  const processedTaskNumbers = new Set();
+  originalXlData.forEach((incident) => {
+    const agent = incident["Taken By"];
+    if (!incidentsByAgent[agent]) {
+      incidentsByAgent[agent] = [];
+    }
+    incidentsByAgent[agent].push(incident);
+  });
+  for (const sfMember in sfAgentMapping) {
+    selectedIncidents[sfMember] = {};
+    sfAgentMapping[sfMember].forEach((agent) => {
+      if (!processedTaskNumbersByAgent[agent]) { 
+        processedTaskNumbersByAgent[agent] = new Set();
+      }
+      
+      selectedIncidents[sfMember][agent] = [];
+      for (const incidentConfig of incidentConfigs) {
+        let currentService =
+          incidentConfig.service !== "RANDOM"
+            ? incidentConfig.service
+            : getRandomService();
+        let currentContactType =
+          incidentConfig.contactType !== "RANDOM"
+            ? incidentConfig.contactType
+            : getRandomContactType();
+        let currentFtf =
+          incidentConfig.ftf !== "RANDOM" ? incidentConfig.ftf : getRandomFtf();
+          let potentialIncidents = incidentsByAgent[agent].filter((incident) => {
+            return (
+              !processedTaskNumbers.has(incident["Task Number"]) && 
+              !processedTaskNumbersByAgent[agent].has(incident["Task Number"]) && 
+              currentService === incident["Service"] &&
+              currentContactType === incident["Contact type"] &&
+              currentFtf === incident["First time fix"]
+            );
+          });
+        let incidentsToAssign = Math.min(
+          potentialIncidents.length,
+          maxIncidents - selectedIncidents[sfMember][agent].length
+        );
+        for (let i = 0; i < incidentsToAssign; i++) {
+          let incident = potentialIncidents[i];
+          selectedIncidents[sfMember][agent].push(incident);
+          processedTaskNumbers.add(incident["Task Number"]); 
+          processedTaskNumbersByAgent[agent].add(incident["Task Number"]);
+        }
+      }
+    });
+  }
+  return selectedIncidents;
+}
+function formatRowsForDownload(selectedIncidents) {
+  const rows = [];
+  let previousSFMember = "";
+  let previousAgent = "";
+  for (const sfMember in selectedIncidents) {
+    for (const agent in selectedIncidents[sfMember]) {
+      selectedIncidents[sfMember][agent].forEach((incident) => {
+        rows.push({
+          "SF Member": previousSFMember === sfMember ? "" : sfMember,
+          Agent: previousAgent === agent ? "" : agent,
+          "Task Number": incident["Task Number"],
+          Service: incident["Service"],
+          "Contact type": incident["Contact type"],
+          "First time fix": incident["First time fix"],
+        });
+        if (previousSFMember !== sfMember) previousSFMember = sfMember;
+        if (previousAgent !== agent) previousAgent = agent;
+      });
+    }
+  }
+  return rows;
+}
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
