020983f9c671433a4fca9b5bc85390796bf7171f
kodemystic <btarrana@protonmail.ch>
2023-10-10 10:06:57 +0200

    1

--- server/app.js
+++ server/app.js
@@ -4,26 +4,24 @@ const xlsx = require("xlsx");
 const bodyParser = require("body-parser");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
+
 const app = express();
-const port = process.env.SERV_PORT;
-const hostname = process.env.HOSTNAME;
+const { UI_PORT: uiPort, SERV_PORT: port, HOSTNAME: hostname, SERV_FILENAME: filename } = process.env;
 const upload = multer({ dest: "uploads/" });
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:8080', `http://${req.hostname}:8080`];
+  const allowedOrigins = [`http://localhost:${uiPort}`, `http://${req.hostname}:${uiPort}`];
   const origin = req.headers.origin;
-  origin && allowedOrigins.includes(origin) ? res.setHeader('Access-Control-Allow-Origin', origin) : null;
+  origin && allowedOrigins.includes(origin) && res.setHeader('Access-Control-Allow-Origin', origin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("Server running!");
-});
+app.get("/", (req, res) => res.send("Server running!"));
 
 let lastUploadedFilePath;
 app.post("/upload", upload.single("file"), (req, res) => {
@@ -37,23 +35,22 @@ app.post("/upload", upload.single("file"), (req, res) => {
 
 app.post("/process", upload.single("file"), async (req, res) => {
   try {
-    const config = req.body;
+    const { body: config } = req;
     const workbook = xlsx.readFile(lastUploadedFilePath);
     const sheetName = workbook.SheetNames[0];
     const originalXlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    const incidentConfigs = config.incidentConfigs;
-    const sfMembers = config.sfMembers;
+    const { incidentConfigs, sfMembers, incidentsPerAgent } = config;
     const incidentsByAgent = mapIncidentsByAgent(originalXlData);
     const sfAgentMapping = mapSFMembersToIncidentAgents(sfMembers, incidentsByAgent);
-    const selectedIncidents = await selectIncidentsByConfiguration(originalXlData, incidentConfigs, config.incidentsPerAgent, sfAgentMapping);
+    const selectedIncidents = await selectIncidentsByConfiguration(originalXlData, incidentConfigs, incidentsPerAgent, sfAgentMapping);
     const rows = formatRowsForDownload(selectedIncidents);
-    if (rows.length < config.incidentsPerAgent) throw new Error("Not enough incidents matched the provided configuration");
+    if (rows.length < incidentsPerAgent) throw new Error("Not enough incidents matched the provided configuration");
     const newFilePath = createAndWriteWorksheet(workbook, rows);
     downloadFile(res, newFilePath);
   } catch (error) {
     console.error("Error in /process:", error);
     console.error("Request body:", req.body);
-    lastUploadedFilePath ? console.error("Last uploaded file path:", lastUploadedFilePath) : null;
+    lastUploadedFilePath && console.error("Last uploaded file path:", lastUploadedFilePath);
     res.status(500).send("Internal Server Error");
   }
 });
@@ -79,27 +76,25 @@ const selectIncidentsByConfiguration = async (originalXlData, incidentConfigs, m
 }
 
 const mapIncidentsByAgent = (originalXlData) => {
-  const incidentsByAgent = {};
-  originalXlData.forEach((incident) => {
+  return originalXlData.reduce((incidentsByAgent, incident) => {
     const agent = incident["Taken By"];
     incidentsByAgent[agent] = incidentsByAgent[agent] ? incidentsByAgent[agent] : [];
     incidentsByAgent[agent].push(incident);
-  });
-  return incidentsByAgent;
+    return incidentsByAgent;
+  }, {});
 }
 
 const mapSFMembersToIncidentAgents = (sfMembers, incidentsByAgent) => {
-  const sfAgentMapping = {};
-  const shuffledAgents = Object.keys(incidentsByAgent).sort(() => 0.5 - Math.random());
-  shuffledAgents.forEach((agent, index) => {
-    const sfMember = sfMembers[index % sfMembers.length];
-    sfAgentMapping[sfMember] = sfAgentMapping[sfMember] ? sfAgentMapping[sfMember] : [];
+  return sfMembers.reduce((sfAgentMapping, sfMember, index) => {
+    const agent = Object.keys(incidentsByAgent)[index % sfMembers.length];
+    sfAgentMapping[sfMember] = sfAgentMapping[sfMember] || [];
     sfAgentMapping[sfMember].push(agent);
-  });
-  return sfAgentMapping;
+    return sfAgentMapping;
+  }, {});
 }
 
 const filterIncidentsByCriterion = (incidents, field, value, agent, alreadySelected) => {
+  incidents = fisherYatesShuffle(incidents);
   value = (value === 'RANDOM') ? getRandomValue(incidents, field) : value;
   const filtered = incidents.filter(incident => !alreadySelected.has(incident) && incident[field] === value && incident['Taken By'] === agent);
   return filtered.length ? filtered : incidents.filter(incident => incident['Taken By'] === agent);
@@ -109,6 +104,14 @@ const getRandomValue = (incidents, field) => {
   const values = [...new Set(incidents.map(incident => incident[field]))];
   return values[Math.floor(Math.random() * values.length)];
 }
+
+const fisherYatesShuffle = array => {
+  array.forEach((element, i) => {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  });
+  return array;
+};
 
 const selectUniqueIncidentForAgent = (filteredIncidents, alreadySelected) => {
   const uniqueIncidents = filteredIncidents.filter(incident => !alreadySelected.has(incident));
@@ -124,30 +127,29 @@ const createAndWriteWorksheet = (workbook, rows) => {
 };
 
 const formatRowsForDownload = (selectedIncidents) => {
-  const rows = [];
   let previousSFMember = "";
   let previousAgent = "";
-  Object.keys(selectedIncidents).forEach(sfMember => {
-    Object.keys(selectedIncidents[sfMember]).forEach(agent => {
-      selectedIncidents[sfMember][agent].forEach((incident) => {
-        rows.push({
+  return Object.keys(selectedIncidents).map(sfMember => {
+    return Object.keys(selectedIncidents[sfMember]).map(agent => {
+      return selectedIncidents[sfMember][agent].map((incident) => {
+        const row = {
           "SF Member": previousSFMember === sfMember ? "" : sfMember,
           Agent: previousAgent === agent ? "" : agent,
           "Task Number": incident["Task Number"],
           Service: incident["Service"],
           "Contact type": incident["Contact type"],
           "First time fix": incident["First time fix"],
-        });
+        };
         previousSFMember = previousSFMember !== sfMember ? sfMember : previousSFMember;
         previousAgent = previousAgent !== agent ? agent : previousAgent;
+        return row;
       });
-    });
-  });
-  return rows;
+    }).flat();
+  }).flat();
 };
 
 const downloadFile = (res, newFilePath) => {
-  res.download(newFilePath, "AskIT - QCH_processed.xlsx", function (err) {
+  res.download(newFilePath, "AskIT - QCH_processed.xlsx", (err) => {
     if (err) throw new Error("Error sending the file: " + err);
   });
 };
