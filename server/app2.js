267c9fbec15fde92487bbbc3dd4242d7fb7b0e22
Verminous <btarrana@protonmail.ch>
2023-10-14 23:16:22 +0200

    1

--- server/app2.js
+++ server/app2.js
@@ -1,4 +1,4 @@
-const express = require("express"), cors = require('cors'),  fs = require('fs'), multer = require("multer"), xlsx = require("xlsx"), bodyParser = require("body-parser"), path = require("path"), app = express(), upload = multer({ dest: "uploads/" });
+const express = require("express"), cors = require('cors'), fs = require('fs'), multer = require("multer"), xlsx = require("xlsx"), bodyParser = require("body-parser"), path = require("path"), app = express(), upload = multer({ dest: "uploads/" });
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 const { UI_PORT: uiPort, SERV_PORT: port, HOSTNAME: hostname, SERV_FILENAME: filename } = process.env;
 
@@ -20,88 +20,12 @@ const {
   fisherYatesShuffle = (array) => { array.forEach((_, i) => { const j = Math.floor(Math.random() * (i + 1));[array[i], array[j]] = [array[j], array[i]]; }); return array; },
   mapIncidentsByAgent = (data) => data.reduce((acc, incident) => { const agent = incident["Taken By"]; acc[agent] = acc[agent] || []; acc[agent].push(incident); return acc; }, {}),
   mapSFMembersToIncidentAgents = (sfMembers, incidentsByAgent) => { const sfAgentMapping = {}, agents = Object.keys(incidentsByAgent); fisherYatesShuffle(agents).forEach((agent, index) => { const sfMember = sfMembers[index % sfMembers.length]; sfAgentMapping[sfMember] = [...(sfAgentMapping[sfMember] || []), agent]; }); return sfAgentMapping; },
-  selectUniqueIncident = (filteredIncidents, alreadySelected) => { const uniqueIncidents = filteredIncidents.filter((incident) => !alreadySelected.has(incident)); return uniqueIncidents.length ? uniqueIncidents[Math.floor(Math.random() * uniqueIncidents.length)] : null; },
   selectUniqueIncidentForAgent = (filteredIncidents = [], alreadySelected, originalIncidents = []) => { let uniqueIncidents = filteredIncidents.filter((incident) => !alreadySelected.has(incident)); let selectedIncident; selectedIncident = uniqueIncidents.length ? uniqueIncidents[Math.floor(Math.random() * uniqueIncidents.length)] : (() => { let remainingOriginals = originalIncidents.filter((incident) => !alreadySelected.has(incident)); return remainingOriginals.length ? remainingOriginals[Math.floor(Math.random() * remainingOriginals.length)] : (console.warn('All original incidents have been selected for the current agent.'), null); })(); selectedIncident ? alreadySelected.add(selectedIncident) : null; return selectedIncident; },
 
-  filterByCriterion = (incidents, field, value, agent, alreadySelected, triedValues = new Set()) => {
-    value = value === "RANDOM" ? getRandomValue(incidents, field, alreadySelected) : value;
-    triedValues.add(value);
-    const filtered = incidents.filter(incident => {
-      const matches = !alreadySelected.has(incident) && incident[field] === value && incident["Taken By"] === agent;
-      return matches;
-    });
-  
-    if (!filtered.length) {
-      const allValues = incidents.map((i) => i[field]);
-      const untriedValues = allValues.filter((value) => !triedValues.has(value));
-      if (untriedValues.length === 0) {
-        return [];
-      }
-      value = getRandomValue(incidents, field, alreadySelected);
-      return filterByCriterion(incidents, field, value, agent, alreadySelected, triedValues);
-    }
-  
-    return filtered;
-  },
+  filterByCriterion = (incidents, field, value, agent, alreadySelected, triedValues = new Set()) => { value = value === "RANDOM" ? getRandomValue(incidents, field, alreadySelected) : value; triedValues.add(value); const filtered = incidents.filter(incident => { const matches = !alreadySelected.has(incident) && incident[field] === value && incident["Taken By"] === agent; return matches; }); if (!filtered.length) { const allValues = incidents.map((i) => i[field]); const untriedValues = allValues.filter((value) => !triedValues.has(value)); if (untriedValues.length === 0) { return []; } value = getRandomValue(incidents, field, alreadySelected); return filterByCriterion(incidents, field, value, agent, alreadySelected, triedValues); } return filtered; },
+  filterIncidentsByFields = (incidents, fieldCriteria, agent, alreadySelected) => { return Object.entries(fieldCriteria).reduce((currentIncidents, [field, value]) => { return filterByCriterion(currentIncidents, field, value, agent, alreadySelected); }, incidents); },
+  selectIncidentsByConfiguration = (originalXlData, incidentConfigs, maxIncidents, sfAgentMapping) => { (!Array.isArray(originalXlData) || !originalXlData.length) && (() => { throw new Error("Invalid originalXlData"); })(); const fieldToConfigKey = { "Service": "service", "Contact type": "contactType", "First time fix": "ftf" }; return Object.entries(sfAgentMapping).reduce((selectedIncidents, [sfMember, agents]) => { selectedIncidents[sfMember] = agents.reduce((agentIncidents, agent) => { const alreadySelected = new Set(); agentIncidents[agent] = Array(maxIncidents).fill().reduce((incidents, _, i) => { const incidentConfig = incidentConfigs[i % incidentConfigs.length]; let fieldCriteria = {}; ["Service", "Contact type", "First time fix"].forEach(field => { const configKey = fieldToConfigKey[field]; if (incidentConfig[configKey]) { fieldCriteria[field] = incidentConfig[configKey]; } }); const potentialIncidents = filterIncidentsByFields(originalXlData, fieldCriteria, agent, alreadySelected); if (potentialIncidents.length) { const uniqueIncident = selectUniqueIncidentForAgent(potentialIncidents, alreadySelected); incidents.push(uniqueIncident); alreadySelected.add(uniqueIncident.incidentId); } else { logToFile(`Warning: No incidents available for fallback for agent ${agent}.`); } logToFile(`selectIncidentsByConfiguration_2 - Selected ${incidents.length} incidents for agent ${agent}`); return incidents; }, []); return agentIncidents; }, {}); return selectedIncidents; }, {}); }
 
-  filterIncidentsByCriterion = (incidents, field, value, agent, alreadySelected, originalIncidents) => {
-    const filtered = filterByCriterion(incidents, field, value, agent, alreadySelected);
-    logToFile(`filterIncidentsByCriterion_1 - Filtered ${filtered.length} incidents by ${field} = ${value} for agent ${agent}`);
-    const selectedIncident = selectUniqueIncident(filtered, alreadySelected);
-    logToFile(`filterIncidentsByCriterion_2 - Selected incident after filtering by ${field} = ${value} for agent ${agent}: ${JSON.stringify(selectedIncident)}`);    if (!selectedIncident && value !== 'RANDOM') {
-      const fallbackFiltered = originalIncidents.filter(incident => incident[field] === value && !alreadySelected.has(incident));
-      return fallbackFiltered.length ? [fallbackFiltered[Math.floor(Math.random() * fallbackFiltered.length)]] : [];
-    }
-    logToFile(`filterIncidentsByCriterion_3 - Selected ${selectedIncident ? 1 : 0} incidents after filtering by ${field} = ${value} for agent ${agent}`);
-    return selectedIncident ? [selectedIncident] : [];
-  },
-
-   filterIncidentsByFields = (incidents, fieldCriteria, agent, alreadySelected) => {
-    return Object.entries(fieldCriteria).reduce((currentIncidents, [field, value]) => {
-        return filterByCriterion(currentIncidents, field, value, agent, alreadySelected);
-    }, incidents);
-},
-
- selectIncidentsByConfiguration = (originalXlData, incidentConfigs, maxIncidents, sfAgentMapping) => {
-    (!Array.isArray(originalXlData) || !originalXlData.length) && (() => {
-        throw new Error("Invalid originalXlData");
-    })();
-
-    const fieldToConfigKey = {
-        "Service": "service",
-        "Contact type": "contactType",
-        "First time fix": "ftf"
-    };
-
-    return Object.entries(sfAgentMapping).reduce((selectedIncidents, [sfMember, agents]) => {
-        selectedIncidents[sfMember] = agents.reduce((agentIncidents, agent) => {
-            const alreadySelected = new Set();
-            agentIncidents[agent] = Array(maxIncidents).fill().reduce((incidents, _, i) => {
-                const incidentConfig = incidentConfigs[i % incidentConfigs.length];
-                let fieldCriteria = {};
-                ["Service", "Contact type", "First time fix"].forEach(field => {
-                    const configKey = fieldToConfigKey[field];
-                    if (incidentConfig[configKey]) {
-                        fieldCriteria[field] = incidentConfig[configKey];
-                    }
-                });
-
-                const potentialIncidents = filterIncidentsByFields(originalXlData, fieldCriteria, agent, alreadySelected);
-                if (potentialIncidents.length) {
-                    const uniqueIncident = selectUniqueIncidentForAgent(potentialIncidents, alreadySelected);
-                    incidents.push(uniqueIncident);
-                    alreadySelected.add(uniqueIncident.incidentId);
-                } else {
-                    logToFile(`Warning: No incidents available for fallback for agent ${agent}.`);
-                }
-                logToFile(`selectIncidentsByConfiguration_2 - Selected ${incidents.length} incidents for agent ${agent}`);
-                return incidents;
-            }, []);
-            return agentIncidents;
-        }, {});
-        return selectedIncidents;
-    }, {});
-},
 } = {};
 
 /* WRITE + DOWNLOAD */
